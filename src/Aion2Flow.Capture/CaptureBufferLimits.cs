fe01fcfd0a0aa5ad271e6ad88a2089b12d2e3ab2
Cloris Chan <chan@cloris.me>
2026-05-11 16:27:37 +0800

    Increase buffer sizes and adjust reassembly limits (#66)

--- src/Aion2Flow.Capture/CaptureBufferLimits.cs
+++ src/Aion2Flow.Capture/CaptureBufferLimits.cs
@@ -2,8 +2,8 @@ namespace Cloris.Aion2Flow.Capture;
 
 internal static class CaptureBufferLimits
 {
-    public const int WinDivertPacketBufferSize = 16 * 1024;
-    public const int StreamTailBufferSize = 64 * 1024;
-    public const int ReassemblyPendingByteLimit = 64 * 1024;
+    public const int WinDivertPacketBufferSize = 70 * 1024;
+    public const int StreamTailBufferSize = 1024 * 1024;
+    public const int ReassemblyPendingByteLimit = 1024 * 1024;
     public const int ReassemblyPendingSegmentLimit = 1024;
 }
